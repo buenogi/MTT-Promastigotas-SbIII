a10fd3e55c89dad90b91e0e3973e62913c8d9b8f
bueno.gi <gislayne.bueno@gmail.com>
2023-08-02 10:13:41 -0300

    EDA and plotting

---
+++ Scripts/01_data_organization.R
@@ -0,0 +1,133 @@
+################################################################################
+######################  Data cleaning and organization #########################
+################################################################################
+
+#Packages
+
+#library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+
+# Loading data 
+
+EXP_piloto <- read.csv(file = "Data/Raw/Piloto.csv", sep = ",")
+
+colnames(EXP_piloto) <-  c("x","x1", "REF", "GSH1","C6", "C7", "C44", "C58", "C67", 
+                           "C73", "C85", "C89", "x2")
+EXP_piloto <- EXP_piloto[,-1]
+EXP_piloto <- EXP_piloto[,-1]
+EXP_piloto <- EXP_piloto[,-11]
+EXP_piloto <- EXP_piloto[-1,]
+EXP_piloto <- EXP_piloto[-7,]
+
+EXP_piloto<- EXP_piloto %>%
+  pivot_longer(cols=c("REF", "GSH1","C6", "C7", "C44", "C58", "C67", "C73", 
+                      "C85", "C89"),
+               names_to='pop',
+               values_to='values')
+
+EXP_piloto$conc <-  rep(c("0.0","4.0",               # tested concentrations
+                          "25.6","160.0","400.0","1000.0"), each =  10)
+
+EXP_piloto$pop <- rep(c("REF","GSH1", "C7", "C6","C44", "C58", "C67", "C73", "C85",
+                        "C89"), # populations ID
+                      times= 6)
+
+EXP_piloto$experiment <- "EXP_piloto" 
+
+
+
+
+
+
+
+
+EXP1 <- read.csv(file = "Data/Raw/EXP1.csv", sep = ",")
+
+colnames(EXP1) <-  c("x","x1", "REF", "GSH1","C6", "C7", "C44", "C58", "C67", 
+                     "C73", "C85", "C89", "x2")
+EXP1 <- EXP1[,-1]
+EXP1 <- EXP1[,-1]
+EXP1 <- EXP1[,-11]
+EXP1 <- EXP1[-1,]
+EXP1 <- EXP1[-7,]
+
+EXP1<- EXP1 %>%
+  pivot_longer(cols=c("REF", "GSH1","C6", "C7", "C44", "C58", "C67", "C73", 
+                      "C85", "C89"),
+               names_to='pop',
+               values_to='values')
+
+EXP1$conc <-  rep(c("0.0","4.0",               # tested concentrations
+                    "25.6","160.0","400.0","1000.0"), each =  10)
+
+EXP1$pop <- rep(c("REF","GSH1", "C7", "C6","C44", "C58", "C67", "C73", "C85",
+                  "C89"), # populations ID
+                times= 6)
+
+EXP1$experiment <- "EXP_1" 
+
+
+# EXP 2
+
+EXP2 <- read.csv(file = "Data/Raw/EXP2.csv", sep = ",")
+
+
+colnames(EXP2) <-  c("x","x1", "REF", "GSH1","C6", "C7", "C44", "C58", "C67",
+                     "C73", "C85", "C89", "x2")
+EXP2 <- EXP2[,-1]
+EXP2 <- EXP2[,-1]
+EXP2 <- EXP2[,-11]
+EXP2 <- EXP2[-1,]
+EXP2 <- EXP2[-7,]
+
+EXP2<- EXP2 %>%
+  pivot_longer(cols=c("REF", "GSH1","C6", "C7", "C44", "C58", "C67", "C73", 
+                      "C85", "C89"),
+               names_to='pop',
+               values_to='values')
+
+EXP2$conc <-  rep(c("0.0","4.0",               # tested concentrations
+                    "25.6","160.0","400.0","1000.0"), each =  10)
+
+EXP2$pop <- rep(c("REF","GSH1", "C7", "C6","C44", "C58", "C67", "C73", 
+                  "C85", "C89"), # populations ID
+                times= 6)
+
+EXP2$experiment <- "EXP_2"                                      # Experiment ID 
+
+EXP3 <- read.csv(file = "Data/Raw/EXP3.csv", sep = ",")
+
+
+colnames(EXP3) <-  c("x","x1", "REF", "GSH1","C6", "C7", "C44", "C58", "C67",
+                     "C73", "C85", "C89", "x2")
+EXP3 <- EXP3[,-1]
+EXP3 <- EXP3[,-1]
+EXP3 <- EXP3[,-11]
+EXP3 <- EXP3[-1,]
+EXP3 <- EXP3[-7,]
+
+EXP3<- EXP3 %>%
+  pivot_longer(cols=c("REF", "GSH1","C6", "C7", "C44", "C58", "C67", "C73", 
+                      "C85", "C89"),
+               names_to='pop',
+               values_to='values')
+
+EXP3$conc <-  rep(c("0.0","4.0",               # tested concentrations
+                    "25.6","160.0","400.0","1000.0"), each =  10)
+
+EXP3$pop <- rep(c("REF","GSH1", "C7", "C6","C44", "C58", "C67", "C73", "C85",
+                  "C89"), # populations ID
+                times= 6)
+
+EXP3$experiment <- "EXP_3"                                      # Experiment ID 
+
+
+#Binding data
+
+DataMTT <- rbind(EXP_piloto,EXP1, EXP2, EXP3)
+
+# Saving processed data
+
+write.csv(DataMTT, file = "Data/Processed/DataMTT")
---
+++ Scripts/02_EDA.R
@@ -0,0 +1,59 @@
+################################################################################
+###################### EDA  - MTT GSH1 HKO clones  #############################
+################################################################################
+
+library(dplyr)
+library(ggplot2)
+
+#Loading data
+
+DataMTT <- read.csv(file = "Data/Processed/DataMTT")
+
+# Checking data
+
+sapply(DataMTT, class)
+DataMTT$pop <- as.factor(DataMTT$pop)
+DataMTT$experiment <- as.factor(DataMTT$experiment) 
+sapply(DataMTT, class)
+
+# Summarazing
+
+DataMTT_sum <- DataMTT%>%
+  group_by(pop,conc)%>%
+  summarise(mean_value = mean(values), sd_value = sd(values))
+
+MTT_plot  <- ggplot(DataMTT, aes(x = log(conc),  y = values,  
+                                 group = experiment))+
+  geom_point(aes(color = experiment))+
+  geom_line(aes(color = experiment))+
+  ggtitle("Promastigotes viability per SbIII dosage") +
+  labs(x = " Conc [   ] μM", y = "Viability (%)")+
+  theme(plot.title = element_text(size = 14,face="bold"),
+        axis.text.x = element_text(size = 10), 
+        axis.text.y = element_text(size = 10),
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15))+
+  #facet_grid(MTT_SbIII_48h$pop)+
+  facet_wrap(DataMTT$pop, nrow = 2)+
+  theme_bw()
+
+MTT_plot +  labs(color = "Populations")
+
+ggsave("Figures/01_MTT_plot.png")
+
+MTT_plot2  <- ggplot(DataMTT_sum, aes(x = log(conc),  y = mean_value,  group = pop))+
+  geom_point(aes(color = pop))+
+  geom_line(aes(color = pop))+
+  ggtitle("Promastigotes viability per SbIII dosage") +
+  labs(x = " Conc [   ] μM", y = "Viability (%)")+
+  theme(plot.title = element_text(size = 14,face="bold"),
+        axis.text.x = element_text(size = 10), 
+        axis.text.y = element_text(size = 10),
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15))+
+  #facet_grid(MTT_SbIII_48h$pop)+
+  #facet_wrap(DataMTT$experiment, nrow = 2)+
+  theme_bw()
+
+MTT_plot2 +  labs(color = "Populations")
+ggsave("Figures/02_MTT_plot.png")
